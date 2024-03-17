511dd4df8414469f049e68298e24b9204abf06be
Lord <148513662+LordDarkdev@users.noreply.github.com>
2024-03-17 13:26:46 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,29 +1,5 @@
 const Discord = require("discord.js");
-const client = new Discord.Client({
-  intents: [
-    1,
-    512,
-    32768,
-    2,
-    128,
-    Discord.IntentsBitField.Flags.DirectMessages,
-    Discord.IntentsBitField.Flags.GuildInvites,
-    Discord.IntentsBitField.Flags.GuildMembers,
-    Discord.IntentsBitField.Flags.GuildPresences,
-    Discord.IntentsBitField.Flags.Guilds,
-    Discord.IntentsBitField.Flags.MessageContent,
-    Discord.IntentsBitField.Flags.Guilds,
-    Discord.IntentsBitField.Flags.GuildMessageReactions,
-    Discord.IntentsBitField.Flags.GuildEmojisAndStickers,
-  ],
-  partials: [
-    Discord.Partials.User,
-    Discord.Partials.Message,
-    Discord.Partials.Reaction,
-    Discord.Partials.Channel,
-    Discord.Partials.GuildMember,
-  ],
-});
+const client = new Client({ intents: 3276799 });
 
 module.exports = client;
 client.commands = new Discord.Collection();
