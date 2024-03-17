0748d8f7322d95112516a2b5d5f6575981b02af0
Lord <148513662+LordDarkdev@users.noreply.github.com>
2024-03-17 16:04:11 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const Discord = require("discord.js");
 const client = new Client({ intents: 3276799 });
-
 module.exports = client;
 client.commands = new Discord.Collection();
 client.aliases = new Discord.Collection();
